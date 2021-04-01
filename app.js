3a43222bc1f7a6c8e24badc8e0c165788751b849
Jairo Espinel <jairo25509@hotmail.com>
2021-03-31 21:49:14 -0500

    Propuestas de navegacion desktop y responsive

--- app.js
+++ app.js
@@ -41,18 +41,58 @@ esp.addEventListener("click", mostrarEsp);
 
 /*STICKY NAVBAR----(ALTERNATIVA DE NAVEGACIÓN)*/
 // When the user scrolls the page, execute myFunction
-// window.onscroll = function () {
-//   myFunction();
-// };
+window.onscroll = function () {
+  myFunction();
+  myFunction2();
+  myFunction3();
+};
 
-// let navbar = document.getElementById("navbar");
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
 
-// let sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
 
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky");
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+/*ESCONDER SIDEBAR AL HACER SCROLL*/
+let menuAesconder = document.getElementById("mySidenav");
+
+function myFunction2() {
+  if (window.pageYOffset >= 600) {
+    closeNav();
+  }
+}
+
+/*STICKY NAVBAR----(ALTERNATIVA DE NAVEGACIÓN MOBILE) */
+let navbarMobile = document.getElementById("mobile-navigation");
+let stickyMobile = navbarMobile.offsetTop;
+
+function myFunction3() {
+  if (window.pageYOffset >= stickyMobile) {
+    navbarMobile.classList.add("sticky-mobile");
+  } else {
+    navbarMobile.classList.remove("sticky-mobile");
+  }
+}
+
+let menuDeOverlay = document.getElementById("mobile-navigation__overlay");
+function overlayIn() {
+  menuDeOverlay.style.transform = "translateX(0)";
+  document.getElementById("body").style.overflow = "hidden";
+}
+
+let hamMobile = document.getElementById("hamburguesa_mobile");
+hamMobile.addEventListener("click", overlayIn);
+
+function overlayOut() {
+  menuDeOverlay.style.transform = "translateX(-100%)";
+  document.getElementById("body").style.overflow = "visible";
+}
+
+let closeMobile = document.getElementById("closebtn-mob");
+closeMobile.addEventListener("click", overlayOut);
+menuDeOverlay.addEventListener("click", overlayOut);
